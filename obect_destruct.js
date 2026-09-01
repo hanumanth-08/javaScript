4313409bf61e2b121f989d24ee6140d5f636896e
hanumanth-08 <marepallyhanumanthreddy@gmail.com>
2026-09-01 10:20:04 +0530

    commit

---
+++ obect_destruct.js
@@ -0,0 +1,14 @@
+const student = {
+    		name: "Rahul",
+   		 age: 20,
+    		course: "JavaScript"
+	};
+
+	const { name, age, course } = student;
+
+	console.log(name);   // Rahul
+	console.log(age);    // 20
+	console.log(course); // JavaScript
+
+
+    
